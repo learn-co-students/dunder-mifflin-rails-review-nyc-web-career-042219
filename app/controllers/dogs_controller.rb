b4d62d9efbfbfb695adc13fa644e6da426905223
cbatsoula <cbatsoula@gmail.com>
2019-05-22 14:28:28 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,49 @@
 class DogsController < ApplicationController
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    @dog.save
+
+      # render :new
+      redirect_to @dog
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+    @dog.update(dog_params)
+    if @dog.save
+      redirect_to @dog
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @dog = Dog.find(params[:id])
+    @dog.destroy
+    redirect_to '/dogs'
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
   resources :dogs
+  delete '/dogs/:id', to: "dogs#destroy", as: "destroydog"
+
+
   resources :employees
+  delete '/employees/:id', to: "employee#destroy", as: "destroyemployee"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
