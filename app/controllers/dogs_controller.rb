beeb6f6c3824160115db7f58eafa8232d36ebcfb
Jessica Lin <jessicaa.lin@gmail.com>
2019-05-22 14:28:31 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,39 @@
 class DogsController < ApplicationController
+  before_action :find_dog, only: [:show]
+  
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @employees = Employee.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def index_by_employee_amount
+    @dogs = Dog.all.sort_by {|dog| dog.employees.length}.reverse
+  end
+
+  private
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
+
+  def find_dog
+    @dog = Dog.find(params[:id])
+  end
 end
